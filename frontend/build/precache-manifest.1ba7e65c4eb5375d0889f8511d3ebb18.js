0c02216e43c9961218d046b74e8462b2f1f5f51e
Vidwan Raj <vidwanraj@yahoo.in>
2021-09-16 18:29:57 -0400

    Add files via upload
    initial changes

---
+++ frontend/build/precache-manifest.1ba7e65c4eb5375d0889f8511d3ebb18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "232701b3b6ea57aa4c5195038489ae75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab0c85ac6988a480cbdf",
+    "url": "/static/css/main.204679ff.chunk.css"
+  },
+  {
+    "revision": "aea3068e9dd67c0816e6",
+    "url": "/static/js/2.a1ec3f3e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.a1ec3f3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab0c85ac6988a480cbdf",
+    "url": "/static/js/main.7aced255.chunk.js"
+  },
+  {
+    "revision": "e72ff7509a54d5946683",
+    "url": "/static/js/runtime-main.372f1ef1.js"
+  }
+]);
